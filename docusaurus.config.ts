fe4d5133b6d4e43e6566411c90cf4bb87872f987
kleeedolinux <kleejulia@yahoo.com>
2026-03-31 16:43:42 -0300

    fixed baseurl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://kleeedolinux.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/DiscordRDA-Doc/',
 
   organizationName: 'kleeedolinux', // Usually your GitHub org/user name.
   projectName: 'discordrda', // Usually your repo name.
